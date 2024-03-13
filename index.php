3e1275eb9062867e29a30b9f59c3933ed17b7245
Djire <ouzairoudjire67@gmail.com>
2024-03-13 12:29:08 +0000

    un envoi vers distant

--- index.php
+++ index.php
@@ -12,7 +12,7 @@
   <div class="sucess">
      <h1>Bienvenue, 
     <p>Vous êtes connecté en tant qu'utilisateur avec l'adresse email 
-   ma modiffication vers le local va s'afficher ici
+  une ligne ajoute
   </div>
 </body>
 
